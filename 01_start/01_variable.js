4f94113b2c693a5068bd0d019e4a350a97aa2a8e
Naitik <naitikraj934@gmail.com>
2026-09-04 05:51:32 +0000

    update variable file

---
+++ 01_start/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=12345
+let accountEmail="naitik@123"
+var accounpassword=12345
+accouncity="G.Noida"
+let accountstate
+
+accountEmail="nr@Nr"
+accounpassword=5555
+accouncity="Noida"
+console.log(accountId)
+
+/* prefer not to use var because of issue in scope and functional scope;
+*/
+
+console.table([accountEmail,accounpassword,accouncity,accountstate])
