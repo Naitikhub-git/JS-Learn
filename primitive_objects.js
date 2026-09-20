1449eca8b26da7b0e45ad74f073b0235f53070be
Naitik <naitikraj934@gmail.com>
2026-09-20 13:38:39 +0000

    printive and objects

---
+++ primitive_objects.js
@@ -0,0 +1,19 @@
+//nn bb ss u
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("567")
+let e="Naitik"
+let f=Symbol("I am a nice symbol")
+let g=undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof b)
+
+
+//objects in js
+const item={
+    "Naitik":true,
+    "Raj":false,
+    "Rhan":undefined
+}
+console.log(item["Naitik"])
