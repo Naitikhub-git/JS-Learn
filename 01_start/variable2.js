e4089447af4a304ecf55b78dbab04386f2d04ffd
Naitik <naitikraj934@gmail.com>
2026-09-20 13:23:26 +0000

    types of variable

---
+++ 01_start/variable2.js
@@ -0,0 +1,16 @@
+let a=67;
+//let is used to declare variable 
+console.log(a);
+a="Naitik";
+console.log(a);
+console.log("Javascript tutorial 3: var ,let and const")
+var b="Raj"
+var b="Kumar"
+let c="Naitik"
+const d="golu"
+{
+c="raj"
+console.log(c);
+}
+console.log(b);
+console.log(c);
