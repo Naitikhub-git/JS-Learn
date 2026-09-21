849894c9f9bed3601967e9a90e3d940eedb0bfe1
Naitik <naitikraj934@gmail.com>
2026-09-21 14:22:29 +0000

    for loop

---
+++ 01_start/forloop.js
@@ -0,0 +1,25 @@
+// const prompt = require("prompt-sync")();
+
+// let n = Number(prompt("Enter n: "));
+// let sum = 0;
+
+// for (let i = 0; i < n; i++) {
+//     sum += i + 1;
+// }
+
+// console.log("Sum =", sum);
+
+let obj={
+    naitik:90,
+    golu:80
+}
+//for in loop
+for(let a in obj){
+    console.log("Marks of "+ a + " are " + obj[a])
+
+}
+//for of loop
+for(let b in "Naitik"){
+    console.log(b)
+
+}
