f68b6281cd33469610c7668cc9c0c728d27454d6
Naitik <naitikraj934@gmail.com>
2026-09-21 14:38:24 +0000

    functions

---
+++ 01_start/function.js
@@ -0,0 +1,12 @@
+function oneplusaverage(x,y){
+    console.log("done")
+    return 1+(a+b)/2;
+}
+
+let a=1;
+let b=2
+let c=3
+
+console.log("one plus average of a and b is ",oneplusaverage(a+b));
+console.log("one plus average of a and b is ",oneplusaverage(b+c));
+console.log("one plus average of a and b is ",oneplusaverage(c+a));
