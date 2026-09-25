3d74c273439296307e10486688f4ae107be5f37a
Naitik <naitikraj934@gmail.com>
2026-09-25 10:27:45 +0000

    learning string

---
+++ Strings/smethods.js
@@ -0,0 +1,11 @@
+let name="Naitik"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,4))
+console.log(name.slice(2))
+console.log(name.replace("t","T"))
+
+let name1="Raj"
+console.log(name.concat("is a friend of ",name1," ok"))
+console.log(name1[0])
---
+++ Strings/string.js
@@ -0,0 +1,19 @@
+//collection of alphabets
+
+let name="Naitik"
+console.log(name)
+console.log(name.length)
+
+let s1='Golu'
+console.log(s1)
+
+//templates literals
+
+let boy1="raj"
+let boy2="Kumar"
+let sentence=`${boy2} is a friend of ${boy1}` 
+console.log(sentence);
+
+//esacpe charaters
+let c1='bana\'na'
+console.log(c1)
