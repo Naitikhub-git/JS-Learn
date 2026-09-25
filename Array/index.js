730c312bfdd8f11be3380d0d7ef4e0fa7c00b0e3
Naitik <naitikraj934@gmail.com>
2026-09-25 11:22:43 +0000

    add

---
+++ Array/index.js
@@ -0,0 +1,3 @@
+let marks=[45,32,56,34,null,"Not present"]
+console.log(marks)
+console.log("The length of marks : "+ marks.length)
---
+++ Array/methods.js
@@ -0,0 +1,9 @@
+let marks=[45,32,56,34]
+let b=marks.toString()
+console.log(b)
+let r=marks.pop()
+console.log(marks,r)
+let s=marks.push(66)
+console.log(marks,s)
+let c=marks.join(" and ")
+console.log(c,typeof c)
